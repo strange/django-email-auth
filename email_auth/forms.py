3e0035010b6291ac318a7c16a94463bdd88286f0
Gustaf Sjöberg <gs@distrop.com>
2009-10-31 11:55:47 +0100

    Wrapped a few long lines.

--- email_auth/forms.py
+++ email_auth/forms.py
@@ -18,12 +18,17 @@ class AuthenticationForm(Form):
             self.user_cache = authenticate(username=email,
                                            password=password)
             if self.user_cache is None:
-                raise forms.ValidationError(_("Please enter a correct email and password. Note that the password is case-sensitive."))
+                raise forms.ValidationError(_("Please enter a correct email "
+                                              "and password. Note that the "
+                                              "password is case-sensitive."))
             elif not self.user_cache.is_active:
                 raise forms.ValidationError(_("This account is inactive."))
 
         if self.request:
             if not self.request.session.test_cookie_worked():
-                raise forms.ValidationError(_("Your Web browser doesn't appear to have cookies enabled. Cookies are required for logging in."))
+                raise forms.ValidationError(_("Your Web browser doesn't "
+                                              "appear to have cookies "
+                                              "enabled. Cookies are required "
+                                              "for logging in."))
 
         return self.cleaned_data
